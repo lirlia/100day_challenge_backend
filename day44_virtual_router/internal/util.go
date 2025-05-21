615af84a12005b95384bab910e50d1debfd377ee
gin <10017674+lirlia@users.noreply.github.com>
2025-05-21 22:27:09 +0900

    remove

--- day44_virtual_router/internal/util.go
+++
@@ -1,4 +0,0 @@
-package internal
-
-// This file can contain utility functions shared within the internal packages of the application.
-// For now, it might remain empty or have very basic helpers.
--- day44_virtual_router/static/js/main.js
+++
@@ -1,1 +0,0 @@
-// Custom JavaScript can be added here
